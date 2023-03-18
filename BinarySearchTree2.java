827ab1beb7a9124f3422b4c6ef1c6ba791890615
ken-sevenseas <inakanookaki@icloud.com>
2023-03-18 21:54:56 +0900

    first commit

---
+++ BinarySearchTree2.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class BinarySearchTree2 {
+    static int N;
+    static Bnode root;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = Integer.parseInt(sc.next());
+        String command;
+        for (int i = 0; i < N; i++) {
+            command = sc.next();
+
+            if (command.equals("insert")) {
+                insert(Integer.parseInt(sc.next()));
+            } else if (command.equals("print")) {
+                inParse(root);
+                System.out.println();
+                preParse(root);
+                System.out.println();
+            } else if (command.equals("find")) {
+                int x = Integer.parseInt(sc.next());
+                Bnode n = find(root, x);
+                if (n != null) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+            }
+        }
+        sc.close();
+    }
+
+    public static Bnode find(Bnode node, int key) {
+        while (node != null && key != node.key) {
+            if (key < node.key) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return node;
+    }
+
+    public static void insert(int key) {
+        Bnode x = root;
+        Bnode y = root;
+        Bnode z = new Bnode();
+
+        z.key = key;
+
+        while (x != null) {
+            y = x;
+            if (z.key < x.key) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+
+        z.parent = y;
+
+        if (y == null) {
+            root = z;
+        } else {
+            if (z.key < y.key) {
+                y.left = z;
+            } else {
+                y.right = z;
+            }
+        }
+    }
+
+    public static void inParse(Bnode node) {
+        if (node == null) {
+            return;
+        }
+        inParse(node.left);
+        System.out.print(" " + node.key);
+        inParse(node.right);
+    }
+
+    public static void preParse(Bnode node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(" " + node.key);
+        preParse(node.left);
+        preParse(node.right);
+    }
+}
+
+class Bnode {
+    int key;
+    Bnode right, left, parent;
+}
