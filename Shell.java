19329dfc32acb97126e73a5ce8d2b85d0112c1e5
ken-sevenseas <inakanookaki@icloud.com>
2023-03-24 21:57:24 +0900

    add: explanation comment

--- Shell.java
+++ Shell.java
@@ -8,26 +8,31 @@ public class Shell {
         Scanner sc = new Scanner(System.in);
         int num = Integer.parseInt(sc.next());
         int[] arr = new int[num];
+
         for (int i = 0; i < num; i++) {
             arr[i] = Integer.parseInt(sc.next());
         }
         sc.close();
+
         int n = shellSort(arr);
         System.out.println(n);
+
         for (int i = n - 1; i >= 0; i--) {
             if (i != n - 1) {
                 System.out.print(" ");
             }
             System.out.print(data[i]);
         }
+
         System.out.println();
         System.out.println(count);
+
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    private static void insertionSort(int[] nums, int num) {
+    private static void insertionSort(int[] nums, int num) {//間隔を指定した挿入ソート
         for (int i = num; i < nums.length; i++) {
             int value = nums[i];
             int j = i - num;
@@ -43,17 +48,20 @@ public class Shell {
     private static int shellSort(int[] nums) {
         int n = 1;
         int i = 0;
+
         while (true) {
-            if (n > nums.length) {
+            if (n > nums.length) {// 間隔が配列より長い場合はbreak
                 break;
             }
-            data[i] = n;
+            data[i] = n; //整列する間隔を格納する。
             n = 3 * n + 1;
             i++;
         }
+
         for (int j = i - 1; j >= 0; j--) {
             insertionSort(nums, data[j]);
         }
+
         return i;
     }
 
