d58da17170f0f937c5b09c30a79f74973589da7b
ken-sevenseas <inakanookaki@icloud.com>
2023-03-25 22:54:04 +0900

    change: array -> arrayList

--- FibonacciNumber2.java
+++ FibonacciNumber2.java
@@ -1,15 +1,21 @@
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FibonacciNumber2 {
-    static int[] dp;
+    //    static int[] dp;
+    static List<Integer> dp = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         PrintWriter pw = new PrintWriter(System.out);
 
         int N = Integer.parseInt(sc.next());
-        dp = new int[N + 1];
+        for (int i = 0; i < N + 1; i++) {
+            dp.add(-1);
+        }
+//        dp = new int[N + 1];
 
         pw.println(fibonacci(N));
         pw.flush();
@@ -18,10 +24,11 @@ public class FibonacciNumber2 {
 
     public static int fibonacci(int n) {
         if (n == 0 || n == 1) {
-            return dp[n] = 1;
+            dp.set(n, 1);
+            return dp.get(n);
         }
-        if (dp[n] != 0) {
-            return dp[n];
+        if (dp.get(n) != -1) {
+            return dp.get(n);
         }
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
