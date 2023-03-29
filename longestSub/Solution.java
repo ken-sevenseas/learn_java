2a9aac0f89dc27d01c66b0a02cceb1b38fe46c65
ken-sevenseas <inakanookaki@icloud.com>
2023-03-29 17:19:04 +0900

    Using: hash map

---
+++ longestSub/Solution.java
@@ -0,0 +1,20 @@
+package longestSub;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);//添え字の更新を行う
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+}
