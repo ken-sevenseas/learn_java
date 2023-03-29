5853db1e2222bd0f4cd85402f444f7da093c8af1
ken-sevenseas <inakanookaki@icloud.com>
2023-03-29 15:16:41 +0900

    add: why while-condition is so

--- AddTwoNumbers/Solution.java
+++ AddTwoNumbers/Solution.java
@@ -6,10 +6,11 @@ class Solution {
         ListNode curr = dummyHead;
         int carry = 0;
 
+        //if both l1 and l2 have the same number, it will be null at the same time but we must carry forward. So, We add it in condition carry != 0.
         while (l1 != null || l2 != null || carry != 0) {
             int x = (l1 != null) ? l1.val : 0;
             int y = (l2 != null) ? l2.val : 0;
-            int sum = carry + x + y;
+            int sum = carry + x + y;//繰り上げ
 
             carry = sum / 10;
             curr.next = new ListNode(sum % 10);
